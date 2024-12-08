9a7888b9c4efd7fb6819cb3e8a32dff9245b9c60
VABILISETTI HARSHA KALYAN NITISH <140156471+HARSHAKALYANNITISH@users.noreply.github.com>
2024-12-08 07:12:37 +0530

    Add files via upload

---
+++ spring_controler/src/main/java/com/klef/spring_controler/SpringControlerApplication.java
@@ -0,0 +1,13 @@
+package com.klef.spring_controler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringControlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringControlerApplication.class, args);
+	}
+
+}
---
+++ spring_controler/src/test/java/com/klef/spring_controler/SpringControlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klef.spring_controler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringControlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
